a0eb888a7d92ffc27756edf2a1c761bdbdc9588d
XiYan <i@xiyan.it>
2026-01-26 15:23:54 +0800

    fix: 修复一些问题

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -31,7 +31,7 @@ const customOAuth: OAuthConfig<any> = {
 
 export const authConfig: NextAuthConfig = {
   adapter: PrismaAdapter(prisma),
-  basePath: "/webvpn-api/auth",
+  basePath: "/webvpn/webvpn-api/auth",
   providers: [
     customOAuth,
     Credentials({
